45f45c14f004dbaf8f92d4b2651591ebfd25f6d2
hilkojj <hilkojj@outlook.com>
2021-01-25 15:35:43 +0100

    fire sounds

--- assets/sounds/attributions.md
+++ assets/sounds/attributions.md
@@ -1,5 +1,6 @@
 Sound effects used in the game:
 
 - [`arrow_shoot.ogg`](https://opengameart.org/content/sfxpunch) **by** (c) copyright Blender Foundation | apricot.blender.org [(CC BY 3.0)](https://creativecommons.org/licenses/by/3.0/)
+- [`fire/fire0.ogg`](https://freesound.org/people/Dynamicell/sounds/17548/) **by** Dynamicell [(CC BY 3.0)](https://creativecommons.org/licenses/by/3.0/) (adapted)
 - not listed files are cc0/public domain
 
--- source/ecs/systems/FireSystem.cpp
+++ source/ecs/systems/FireSystem.cpp
@@ -4,6 +4,7 @@
 #include "../../generated/Fire.hpp"
 #include "../../generated/Physics.hpp"
 #include <generated/Spawning.hpp>
+#include <generated/SoundSpeaker.hpp>
 
 void FireSystem::update(double deltaTime, EntityEngine *engine)
 {
@@ -12,7 +13,17 @@ void FireSystem::update(double deltaTime, EntityEngine *engine)
 
         for (int i = 0; i < fire.intensity * 2 * (1 + fire.width * .8) * mu::random(mu::random(mu::random())); i++)
         {
-            auto particle = engine->createChild(e);
+            auto particle = engine->entities.create();
+
+            bool sound = mu::random() > .98;
+            if (sound)
+            {
+                SoundSpeaker &sound = engine->entities.assign_or_replace<SoundSpeaker>(particle);
+                sound.sound.set("sounds/fire/fire0");
+//                sound.volume = .6; set in update loop below
+                sound.pitch = mu::random(.5, 1.5);
+            }
+
             engine->entities.assign<FireParticle>(particle);
             AABB &particleAABB = engine->entities.assign<AABB>(particle);
             particleAABB.center = aabb.center;
@@ -23,7 +34,12 @@ void FireSystem::update(double deltaTime, EntityEngine *engine)
             particlePhysics.gravity = mu::random(-280, -50);
             particlePhysics.moveByWind = 500;
             particlePhysics.velocity = vec2(mu::random(-40, 40) * mu::random(.2, 1.2), mu::random(-50, 100));
-            engine->entities.assign<DespawnAfter>(particle).time = log(fire.intensity + 1) * mu::random(.5, 1.4);
+            engine->entities.assign<DespawnAfter>(particle).time = log(fire.intensity + 1) * mu::random(.5, 1.4) * (sound ? 3. : 1.);
         }
+    });
+
+    engine->entities.view<FireParticle, SoundSpeaker, DespawnAfter>().each([&](auto, SoundSpeaker &sound, DespawnAfter &despawn) {
+        float age = despawn.timer / despawn.time;
+        sound.volume = min(1. - age, age * 2.) * .1;
     });
 }
